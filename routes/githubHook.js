409375dfd66820f99816eb1d0aafc1c8b015cb01
Mitchell Jeppson <mitch9300@hotmail.com>
2019-04-02 18:43:08 -0400

    add github hook deployment

--- routes/githubHook.js
+++ routes/githubHook.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+var shell = require('shelljs');
 
 const GithubWebHook = require('express-github-webhook');
 const webhookHandler = GithubWebHook({ path: '/', secret: 'MitchellJamesJeppson' });
@@ -7,7 +8,11 @@ const webhookHandler = GithubWebHook({ path: '/', secret: 'MitchellJamesJeppson'
 router.use(webhookHandler); // use our middleware
 
 webhookHandler.on('push', function (event, repo, data) {
-    console.log(event);
+    shell.exec('git fetch origin master');
+    shell.exec('git reset --hard FETCH_HEAD');
+    shell.exec('rm -r node_models');
+    shell.exec('npm install');
+    console.log('Deployment successfull');
 });
 
 module.exports = router;
